7e95273ea5767d0cfba18ef872fcae26599e2d69
bskyb-mysky <mysky@bskyb.com>
2013-09-14 18:42:41 +0100

    enabling retrieving fixtures and tidy up

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -4,28 +4,24 @@ class PredictionsController < ApplicationController
 
   def new
     @selected_teams = ['West Ham', 'Chelsea', 'Arsenal', 'Man City', 'Man Utd', 'Liverpool', 'Tottenham']
-    #fixture_service = FixtureService.new
-    #@fixtures = fixture_service.retrieve_future_fixtures
-    @fixtures = [{:kick_off=>"Saturday 14 September 2013 12:45:00", :home_team=>"Man Utd", :away_team=>"Crystal Palace"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Aston Villa", :away_team=>"Newcastle"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Fulham", :away_team=>"West Brom"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Hull", :away_team=>"Cardiff"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Tottenham", :away_team=>"Norwich"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Stoke", :away_team=>"Man City"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Sunderland", :away_team=>"Arsenal"}, {:kick_off=>"Saturday 14 September 2013 17:30:00", :home_team=>"Everton", :away_team=>"Chelsea"}, {:kick_off=>"Sunday 15 September 2013 16:00:00", :home_team=>"Southampton", :away_team=>"West Ham"}, {:kick_off=>"Monday 16 September 2013 20:00:00", :home_team=>"Swansea", :away_team=>"Liverpool"}]
+    if Rails.env == :production
+      fixture_service = FixtureService.new
+      @fixtures = fixture_service.retrieve_future_fixtures
+    else
+      @fixtures = [{:kick_off=>"Saturday 14 September 2013 12:45:00", :home_team=>"Man Utd", :away_team=>"Crystal Palace"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Aston Villa", :away_team=>"Newcastle"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Fulham", :away_team=>"West Brom"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Hull", :away_team=>"Cardiff"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Tottenham", :away_team=>"Norwich"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Stoke", :away_team=>"Man City"}, {:kick_off=>"Saturday 14 September 2013 15:00:00", :home_team=>"Sunderland", :away_team=>"Arsenal"}, {:kick_off=>"Saturday 14 September 2013 17:30:00", :home_team=>"Everton", :away_team=>"Chelsea"}, {:kick_off=>"Sunday 15 September 2013 16:00:00", :home_team=>"Southampton", :away_team=>"West Ham"}, {:kick_off=>"Monday 16 September 2013 20:00:00", :home_team=>"Swansea", :away_team=>"Liverpool"}]
+    end
     @fixtures = @fixtures.select do |fixture|
       @selected_teams.include?(fixture[:home_team]) || @selected_teams.include?(fixture[:away_team])
     end
     append_data_to_fixtures
 
-    #Player.delete_all
     #populate_players
 
-    #Prediction.delete_all
-    #create_predictions
     @predictions = Prediction.includes(:user, :prediction_status)
-    evaluate_predictions
+    evaluate_predictions if Rails.env == :production
 
     @display_name = current_user.email
-
     calculate_standings
-
-    #puts '*****************************************'
-    #puts @predictions
   end
 
   def create
@@ -41,26 +37,6 @@ class PredictionsController < ApplicationController
 
   private
 
-  def create_predictions
-
-    Prediction.create([{:home_team => 'Arsenal', :away_team => 'Tottenham', :home_team_score => 0,
-                        :away_team_score => 0, :goal_scorer => 'no scorer', :kick_off => "2013-09-01 16:00:00.000000",
-                        :user_id => 1, :prediction_status_id => 0},
-                       {:home_team => 'Liverpool', :away_team => 'Man Utd', :home_team_score => 2,
-                        :away_team_score => 2, :goal_scorer => 'Daniel Sturridge', :kick_off => "2013-09-01 13:30:00.000000",
-                        :user_id => 1, :prediction_status_id => 0},
-                       {:home_team => 'Arsenal', :away_team => 'Tottenham', :home_team_score => 1,
-                        :away_team_score => 0, :goal_scorer => 'Daniel Sturridge', :kick_off => "2013-09-01 16:00:00.000000",
-                        :user_id => 2, :prediction_status_id => 0},
-                       {:home_team => 'Liverpool', :away_team => 'Man Utd', :home_team_score => 1,
-                        :away_team_score => 0, :goal_scorer => 'Daniel Sturridge', :kick_off => "2013-09-01 13:30:00.000000",
-                        :user_id => 2, :prediction_status_id => 0}
-
-                      ])
-
-
-  end
-
   def calculate_standings
     @standings = []
     User.all.each do |user|
@@ -74,7 +50,6 @@ class PredictionsController < ApplicationController
     end
     @standings.sort! { |x, y| x[:points] <=> y[:points] }.reverse!
   end
-
 
   def prediction_params
     count = 0
@@ -96,41 +71,23 @@ class PredictionsController < ApplicationController
   end
 
   def evaluate_predictions
-    #puts '****************************************************************'
     result_available_after = 2
     unchecked_predictions = @predictions.where("kick_off < ? AND prediction_status_id = 0", Time.now + result_available_after.hours)
     .order("kick_off ASC")
 
     if unchecked_predictions.length > 0
-      #puts "oldest prediction kick off #{unchecked_predictions.last.kick_off}"
-      #puts "number of predictions #{unchecked_predictions.length}"
 
       results = ResultService.new.retrieve_results unchecked_predictions.last.kick_off
-
-      #results =
-      #    {:Arsenal_v_Tottenham => {:home_team_score => 1, :away_team_score => 0, :goal_scorers => ["Olivier Giroud"]},
-      #     :Liverpool_v_Man_Utd => {:home_team_score => 1, :away_team_score => 0, :goal_scorers => ["Daniel Sturridge"]},
-      #     :West_Brom_v_Swansea => {:home_team_score => 0, :away_team_score => 2, :goal_scorers => ["Ben Davies", "Pablo Hernandez"]}
-      #    }
 
       unchecked_predictions.each do |prediction|
         results_key = (prediction.home_team + " v " + prediction.away_team).gsub!(" ", "_").to_sym
         result = results[results_key]
         if !result.nil?
-          #puts "****************************"
-          #puts "predicted home score #{prediction.home_team_score}"
-          #puts "actual home score #{result[:home_team_score]}"
-          #puts "predicted away score #{prediction.away_team_score}"
-          #puts "actual away score #{result[:away_team_score]}"
-          #puts "predicted scorer #{prediction.goal_scorer}"
-          #puts "actual scorer #{result[:goal_scorers]}"
-
           correct_scoreline = prediction.home_team_score == result[:home_team_score] && prediction.away_team_score == result[:away_team_score]
           correct_scorer = result[:goal_scorers].include?(prediction.goal_scorer) || (result[:goal_scorers].empty? && prediction.goal_scorer == 'no scorer')
 
           prediction.update(calculate_points correct_scoreline, correct_scorer)
         end
-
       end
     end
   end
